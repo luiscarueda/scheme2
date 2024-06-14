cc66754f109f8c90974f908a31c8e5aeb1b16f80
luiscarlosrueda <luiscrueda.ing@gmail.com>
2024-06-14 10:43:30 -0500

    inicializacion

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { About,Header,Testimonials,Services,Experience,Contact } from './containers'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <section>
+      <About/>
+      <Header/>
+      <Testimonials/>
+      <Services/>
+      <Experience/>
+      <Contact/>
+    </section>
+
+    
+  )
+}
+
+export default App
